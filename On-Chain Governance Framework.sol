af8b3aa45288ce7536f0e1c40d72cbeac3654116
PG-bit997 <pragatigaykwad7@gmail.com>
2025-05-16 15:55:21 +0530

    Update On-Chain Governance Framework.sol

--- On-Chain Governance Framework.sol
+++ On-Chain Governance Framework.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract SimpleContract {
+    // State variable to store a number
+    uint256 private storedNumber;
+    
+    // Event to notify when the number is updated
+    event NumberUpdated(uint256 newNumber);
+    
+    // Constructor to initialize the contract
+    constructor() {
+        storedNumber = 0;
+    }
+    
+    // Function 1: Increments the stored number by 1
+    function increment() public returns (uint256) {
+        storedNumber += 1;
+        emit NumberUpdated(storedNumber);
+        return storedNumber;
+    }
+    
+    // Function 2: Returns the current stored number
+    function getNumber() public view returns (uint256) {
+        return storedNumber;
+    }
+}
