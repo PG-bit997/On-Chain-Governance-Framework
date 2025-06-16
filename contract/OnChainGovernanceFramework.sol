787c443537c332151afed871c4b61d01763722fd
PG-bit997 <pragatigaykwad7@gmail.com>
2025-06-16 18:36:49 +0530

    Update OnChainGovernanceFramework.sol

--- contract/OnChainGovernanceFramework.sol
+++ contract/OnChainGovernanceFramework.sol
@@ -12,7 +12,7 @@ contract OnChainGovernanceFramework is Ownable, ReentrancyGuard {
     bool public tokenLocked = fals
 
     Counters.Counter private
-
+\\\\
     
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
